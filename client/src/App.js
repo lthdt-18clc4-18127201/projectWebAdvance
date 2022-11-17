d304b9984ba57d2a8f428c1f4d88927e42b730dc
Pham Tan Su <lthdt-18clc4-18127201>
2022-11-17 19:39:07 +0700

    fix eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <HomePage />
     </div>
   );
